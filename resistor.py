f4236c3723467176ff8d457e970c6d6a53fbd026
Mustafa Anwar <mustafa_anwar_hamza@outlook.com>
2018-04-03 03:35:49 +0200

    First

---
+++ resistor.py
@@ -0,0 +1,19 @@
+#This to make resistor
+List = [[0,0]]
+NewList = []
+a = 0
+lis=[]
+def resistor_s(a, n): 
+
+    List.append(a) 
+    if len(List) >= 1:
+        print(List)
+        lis.append(List[n])      
+        print ("it's : ",sum(lis))
+    
+resistor(480, 1)
+resistor(300, 2)
+resistor(220, 3)
+#print("a : " , a)
+
+
