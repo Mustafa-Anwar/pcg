6065f97c5182695ee0c19f4a0b90b96851a6a1c1
Mustafa Anwar <mustafa_anwar_hamza@outlook.com>
2018-04-03 20:08:06 +0200

    Hello

--- resistor.py
+++ resistor.py
@@ -1,16 +1,36 @@
-#This to make resistor
-List = [[0,0]]
-NewList = []
-a = 0
-lis=[]
-def resistor_s(a, n): 
+#This to make resistor  
 
+#the lists
+List = [] 
+lis= []
+
+def resistor_s(a, n):
     List.append(a) 
-    if len(List) >= 1:
-        print(List)
+    if len(List) >= 1 :
         lis.append(List[n])      
-        print ("it's : ",sum(lis))
-    
-resistor(480, 1)
-resistor(300, 2)
-resistor(220, 3)
+        return (sum(lis))
+
+List2 = []
+lis2 = []
+R1, R2 = 0, 0
+
+def resistor_p(a, n):
+    List2.append(a)
+
+    if len(List2) == 2:
+        R1 = List2[0]
+        R2 = List2[1]
+        r = (R1 * R2) / (R1 + R2) 
+        return r
+    elif len(List2) >= 3:
+        print(List2)
+        for i in range(len(List2)):
+            lis2.append(1 / List2[i])
+        r = 1 / sum(lis2)
+        return r
+
+#test
+#resistor_p(220,0)
+#print(resistor_p(360,1))
+#resistor_s(220,0)
+#print(resistor_s(360,1))
